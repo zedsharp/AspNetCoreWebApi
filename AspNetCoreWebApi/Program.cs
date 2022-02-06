2b023cd19cbc6b6fef4c72ec3d1f02930140ce5f
Zed Sharp <zedsharp@users.noreply.github.com>
2022-02-06 09:41:26 -0500

    enabling Swagger in non-dev and adding CORS

--- AspNetCoreWebApi/Program.cs
+++ AspNetCoreWebApi/Program.cs
@@ -2,6 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins(
+                "http://aspnetcorewebapi.onrender.com/",
+                "https://aspnetcorewebapi.onrender.com/",
+                "http://blazorwasmsample.onrender.com/",
+                "https://blazorwasmsample.onrender.com/");
+        }
+    );
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -12,10 +26,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     //app.UseHttpsRedirection();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors();
 
 app.UseAuthorization();
 
